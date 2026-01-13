d52c120931cb309c74d542e8c54ca75d61619976
Tu Nombre <tu@email.com>
2026-01-12 20:30:55 -0600

    Configuración final de Next.js y estilos

---
+++ tailwind.config.ts/tailwind.config.ts
@@ -0,0 +1,21 @@
+'use client';
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyan: {
+          400: '#22d3ee',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
